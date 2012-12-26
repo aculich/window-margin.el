be2152fdd77b1e509eca8059275609b678594e81
Aaron Culich <aculich@gmail.com>
2012-12-25 23:23:54 -0800

    initial checkin

---
+++ window-margin.el
@@ -0,0 +1,90 @@
+(defgroup window-margin nil
+  "Restrict visual width of window using margins for `visual-line-mode'."
+  :group 'visual-line
+  :version "24.1.1")
+
+(defvar window-margin-width nil
+  "Restrict visual width of window using margins for `visual-line-mode'.
+If non-nil use value of `fill-column', otherwise if an integer
+will be a fixed value, or if a floating point number then a
+percentage of the frame width.")
+(make-local-variable 'window-margin-width)
+(setq-default window-margin-width nil)
+
+(defcustom window-margin-mode-line-string " wm"
+  "String displayed on the modeline when window-margin is active.
+Set this to nil if you don't want a modeline indicator."
+  :group 'window-margin
+  :type '(choice string (const :tag "None" nil)))
+
+(defun window-margin-calculate-minimum-width (&optional width)
+  (cond ((numberp width) width)
+        ((not (null width))
+         (or fill-column (default-value 'window-min-width)))
+        (t (or (default-value 'window-min-width)
+               window-safe-min-width))))
+
+;;;###autoload
+(define-minor-mode window-margin-mode
+  "Restrict visual width of window using margins for `visual-line-mode'.
+With a prefix argument ARG, enable Window Margin mode if ARG is
+positive, and disable it otherwise.  If called from Lisp, enable
+the mode if ARG is omitted or nil.
+
+When Window Margin mode is enabled, the visual width of windows
+will be restricted using margins in `visual-line-mode' (which
+will also be turned on for the selected buffer).
+"
+  :lighter window-margin-mode-line-string
+  :group 'window-margin
+  (if window-margin-mode
+      (progn
+        (unless window-margin-width
+          (setq window-margin-width t))
+        (add-hook 'window-configuration-change-hook 'window-margin-update t t)
+        (turn-on-visual-line-mode)
+        (window-margin-update))
+    (progn
+      (setq window-margin-width nil)
+      (remove-hook 'window-configuration-change-hook 'window-margin-update t)
+      (set-window-margins (get-buffer-window) nil nil))))
+
+;;;###autoload
+(defun turn-on-window-margin-mode ()
+  (window-margin-mode 1))
+(custom-add-option 'text-mode-hook 'turn-on-window-margin-mode)
+
+;;;###autoload
+(defun turn-off-window-margin-mode ()
+  (window-margin-mode -1))
+
+(defun window-margin-update ()
+  (let* ((window-configuration-change-hook
+          (remove 'window-margin-update window-configuration-change-hook))
+         (window (selected-window))
+         (buffer-width
+          (cond ((integerp window-margin-width)
+                 window-margin-width)
+                ((floatp window-margin-width)
+                 window-margin-width)
+                ((and window-margin-width)
+                 (or fill-column nil))
+                (t 80))))
+    (unless (active-minibuffer-window)
+      (set-window-margins window nil nil)
+      (if (window-at-side-p window 'right)
+          (let ((rm (+ (- (window-total-width) buffer-width)
+                       (- (window-body-width) (window-total-width) 1))))
+            (when (> rm 0)
+              (set-window-margins window nil rm)))
+        (let ((window-delta (+ (- buffer-width (window-total-width))
+                               (- (window-total-width) (window-body-width))
+                               1)))
+          (window-resize window window-delta t))))))
+
+(defadvice set-fill-column (after window-margin-update activate)
+  (window-margin-update))
+
+(provide 'window-margin)
+
+;;; window-margin.el ends here
