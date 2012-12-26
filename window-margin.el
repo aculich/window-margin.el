0a27529d8ca79dc31611d43f6490fb6bdea3fc41
Aaron Culich <aculich@gmail.com>
2012-12-25 23:36:05 -0800

    added commentary

--- window-margin.el
+++ window-margin.el
@@ -25,6 +25,25 @@
 
 ;;; Commentary:
 
+;; This minor mode will automatically resize windows to the width of
+;; the fill-column, or optionally to some fixed size set with the
+;; window-margin-width variable.
+;;
+;; To enable it with text-mode use:
+;;
+;;   (add-hook 'text-mode 'turn-on-window-margin-mode)
+;;
+;; This minor mode was inspired by reading an entry on StackOverflow
+;;
+;;   http://stackoverflow.com/questions/14009223/set-column-width-for-visual-lines-in-emacs
+;;
+;; when I discovered that (the quirky, but useful) longlines-mode was
+;; being discontinued. I can't claim that this mode will be any less
+;; quirky (probably moreso at this early 0.1 release), but it makes
+;; use of margins and visual-line mode which is better way to
+;; accomplish the effect than implemented by longlines-mode
+
+
 ;;; Code:
 
 (defgroup window-margin nil
