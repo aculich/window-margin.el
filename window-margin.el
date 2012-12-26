093e8fe7575c6086f93ad319d4db2bbcbe65c6a2
Aaron Culich <aculich@gmail.com>
2012-12-25 23:24:27 -0800

    added copyright notice

--- window-margin.el
+++ window-margin.el
@@ -1,3 +1,32 @@
+;;; window-margin.el --- automatic margins for visual-line-mode wrapping
+
+;; Copyright (C) 2012 by Aaron Culich
+
+;; Maintainer: Aaron Culich <aculich@gmail.com>
+;; Version: 0.1
+;; Keywords: margins, text, visual-line, word wrap
+;; URL: http://github.com/aculich/window-margin.el
+;; Description: automatic margins for visual-line-mode wrapping
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
 (defgroup window-margin nil
   "Restrict visual width of window using margins for `visual-line-mode'."
   :group 'visual-line
